5f1aadbcfd250e75f89992c3dd090949f5dfad54
trannthany <trannthany@gmail.com>
2020-12-09 18:25:52 +1300

    add continue key word

--- break.py
+++ break.py
@@ -4,9 +4,16 @@ for x in a:
         break
     print(x)
 
+print('-------------------')
 i = 0
 while i < 5:
     if i == 3:
         break
     print(i)
     i+=1
+
+print('-------------------')
+for x in a:
+    if x == 2:
+        continue # continue will not execute any code that come after it but go to the next iteration (skipping)
+    print(x)
