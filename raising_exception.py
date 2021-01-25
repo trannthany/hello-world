d2106b736c00bc69b48d58d341375a527122f3b1
trannthany <trannthany@gmail.com>
2021-01-25 13:52:59 +1300

    raise exception

--- raising_exception.py
+++ raising_exception.py
@@ -4,3 +4,13 @@
 # print(2) # this statement won't get executed
 
 
+# raise exception can be raised with arguments that give detail about them
+# name = "123"
+# raise NameError("Invalid name!")
+
+# in except blacks, the raise statement can be used without arguments to reraise whatever exception occurred
+try:
+    num = 5/0
+except:
+    print("An error occurred")
+    raise
