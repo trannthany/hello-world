8d8812df6fac50cebd863fb66389d1c1f79426f1
trannthany <trannthany@gmail.com>
2021-01-27 16:38:24 +1300

    object programming 1

---
+++ object_oriented_programming.py
@@ -0,0 +1,15 @@
+# a sample class
+class PartyAnimal:
+    x = 0 #data
+    def party(self):
+        self.x = self.x + 1
+        print("So far", self.x)
+
+an = PartyAnimal() # instanciate an object
+an.party()
+an.party()
+an.party()
+
+# dir() and type() allows to inspect the object
+print(dir(an))
+print(type(an))
---
+++ regular_expression.py
@@ -0,0 +1,3 @@
+#https://www.dataquest.io/blog/regex-cheatsheet/
+
+import re #before we can use regular expression
